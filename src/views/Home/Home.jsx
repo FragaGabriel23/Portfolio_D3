d837c4a41862c439561f0c97707b2007b92ff64e
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-03-18 09:21:18 -0300

    feat( views/home): Header finished

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,27 @@
 import './index.scss'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <header className='header'>
+            <div className="header__logo">
+                <p>DNC</p>
+            </div>
+            <main className='header__main'>
+
+                <ul className='header__navBar'>
+                    <li><a href="">home</a></li>
+                    <li><a href="">projetos</a></li>
+                    <li><a href="">sobre mim</a></li>
+                    <li><a href="">contato</a></li>
+                </ul>
+
+                <div className='header__text'>
+                    <h1> front-end.web(developer) </h1>
+                </div>
+
+            </main>
+        </header>
+    )
 }
 
 export default Home
