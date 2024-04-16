2b9c6f3d9d995aee06cc79cf518dac6d93735e20
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-04-16 16:52:22 -0300

    feat( Projects/ProjectCard): Created Link and hover in ProjectCard component

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,7 +4,9 @@ const ProjectCard = ({ Project }) => {
     return (
         < article className='cardProject' >
             <div className='cardProject__left'>
-                <img src={Project.img} alt="Print do Projeto" />
+                <a href={Project.link} target='_blank'>
+                    <img src={Project.img} alt="Print do Projeto" />
+                </a>
                 <div className='cardProject__tags'>
                     {Project.tools.map((tool, i) => (<p key={i}>{tool}</p>))}
                 </div>
