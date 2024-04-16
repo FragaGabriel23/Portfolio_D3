a2f3e97347cc331516313d2e865ba7cb9ace8c30
FragaGabriel23 <gabrielfraga2308@gmail.com>
2024-04-16 16:25:51 -0300

    feat: mock projects

---
+++ src/mocks/projects.mock.js
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        tools: ["React.JS", "SASS"],
+        title: "e-Commerce DNC",
+        description: `Interface de compra online para DNC Store, incluindo login,
+                     páginas principais, detalhes do produto, checkout e otimizado 
+                     para dispositivos móveis e desktops.`,
+        img: "/ecommerce.jpg",
+        link: "https://e-commercednc.netlify.app/"
+    },
+    {
+        tools: ["Figma"],
+        title: "DeliveryApp",
+        description: `Protótipo média fidelidade no Figma para um app de delivery. 
+                    Interface intuitiva e funcionalidades essenciais para uma experiência 
+                    de usuário fiel à proposta do aplicativo.`,
+        img: "/delivery.jpg",
+        link: "https://www.figma.com/file/cJKADmwBDF5k9Qk2snApBj/DeliveryApp?type=design&node-id=1%3A2&mode=design&t=BsLrhWWdiaUypjEb-1"
+    },
+    {
+        tools: ["JavaScript", "Bootstrap"],
+        title: "DNC Coffee",
+        description: `Landing page responsiva da DNC Coffee com design baseado no Figma e 
+                    utilizando elementos Bootstrap. Inclui modal informativo e estrutura 
+                    organizada de HTML, CSS e JavaScript.`,
+        img: "/dnccoffee.jpg",
+        link: "https://project-dnc-coffee.netlify.app/"
+    },
+    {
+        tools: ["JavaScript", "HTML", "CSS"],
+        title: "Landing Page DNC",
+        description: `Landing page para empresa de arquitetura com integração de formulário 
+                    para contato de clientes via Sheet Monkey. Responsivo e compatível com 
+                    navegadores modernos.`,
+        img: "/arquitetura.jpg",
+        link: "https://projeto1arquiteturadnc.netlify.app/"
+    },
+]
